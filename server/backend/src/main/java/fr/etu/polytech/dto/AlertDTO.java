ffbb76553a5a25c2946743116109330aef1e2fe1
Anna <annadiplacido3@gmail.com>
2024-10-09 11:47:32 +0200

    feat : start alert gestion

---
+++ server/backend/src/main/java/fr/etu/polytech/dto/AlertDTO.java
@@ -0,0 +1,3 @@
+package fr.etu.polytech.dto;
+
+public record AlertDTO(String type, String message) {}
---
+++ server/backend/src/main/java/fr/etu/polytech/entity/Alert.java
@@ -0,0 +1,20 @@
+package fr.etu.polytech.entity;
+
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Alert extends PanacheMongoEntity {
+    public String type;
+    public String message;
+
+    public Alert() {
+    }
+
+    public Alert(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+}
