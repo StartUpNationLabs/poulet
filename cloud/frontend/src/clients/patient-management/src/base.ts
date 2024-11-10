53273bffd291a395cc50391391e4aeefbb9d2b42
Anna <annadiplacido3@gmail.com>
2024-11-10 22:14:27 +0100

    fix : url in base.ts

--- cloud/frontend/src/clients/patient-management/src/base.ts
+++ cloud/frontend/src/clients/patient-management/src/base.ts
@@ -19,7 +19,7 @@ import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 
-export let BASE_PATH = "http://localhost:51260".replace(/\/+$/, "");
+export let BASE_PATH = "http://localhost:8083".replace(/\/+$/, "");
 
 fetch("/config/env").then(
     (response) => {
