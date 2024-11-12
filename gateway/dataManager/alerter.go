90a349d0655d259b9ea2e40f55dea53f9694aba9
Yvann PONCE <yvann.ponce@gmail.com>
2024-11-12 20:06:44 +0100

    increase acc seuil

--- gateway/dataManager/alerter.go
+++ gateway/dataManager/alerter.go
@@ -75,7 +75,7 @@ func (alerter *Alerter) CheckHealthParameter(param string, sample Sample) bool {
 		isAbnormal = sample.Value < 36.0 || sample.Value > 38
 		message = fmt.Sprintf("Alert! Abnormal %s: %.2f°C", param, sample.Value)
 	case "acceleration":
-		isAbnormal = sample.Value > 100
+		isAbnormal = sample.Value > 500
 		message = fmt.Sprintf("Alert! Abnormal %s: %.2f m/s²", param, sample.Value)
 	case "glucose":
 		isAbnormal = sample.Value < 50 || sample.Value > 140
